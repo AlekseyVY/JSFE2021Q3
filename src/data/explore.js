3da65e4db1316a5a955c1472cb98e7aac34904a7
AlekseyVY <alekseyvy@gmail.com>
2021-09-19 13:38:12 +0300

    feat: explore section 1920 hardcoded draft

--- src/data/explore.js
+++ src/data/explore.js
@@ -5,7 +5,70 @@ const exploreContainer = {
   id: 'explore',
   img: null,
   link: null,
-  content: []
+  content: [{
+    tag: 'div',
+    className: 'explore-content-wrapper',
+    id: null,
+    img: null,
+    link: null,
+    content: [{
+      tag: 'div',
+      className: 'explore-content-text-wrapper',
+      id: null,
+      img: null,
+      link: null,
+      content: [{
+        tag: 'h2',
+        className: 'explore-content-text-logo',
+        id: null,
+        img: null,
+        link: null,
+        content: ['picture explore']
+      },
+        {
+          tag: 'div',
+          className: 'explore-content-text-delimeter',
+          id: null,
+          img: null,
+          link: null,
+          content: []
+        },
+        {
+          tag: 'p',
+          className: 'explore-content-text-sec-text',
+          id: null,
+          img: null,
+          link: null,
+          content: ['Las Meninas is a 1656 painting by Diego Velázquez, the leading artist of the Spanish Golden Age.']
+        },
+        {
+          tag: 'p',
+          className: 'explore-content-text-sec-text',
+          id: null,
+          img: null,
+          link: null,
+          content: ['It was cleaned in 1984 to remove a "yellow veil" of dust that had gathered since the previous restoration in the 19th century.']
+        },
+        {
+          tag: 'p',
+          className: 'explore-content-text-sec-text',
+          id: null,
+          img: null,
+          link: null,
+          content: ['The cleaning provoked furious protests, not because the picture had been damaged in any way, but because it looked different.']
+        }
+      ]
+    },
+      {
+        tag: 'img',
+        className: 'explore-content-img',
+        id: null,
+        img: './assets/explore/img.png',
+        alt: 'explore',
+        link: null,
+        content: []
+      }]
+  }]
 }
 
 export default exploreContainer;
