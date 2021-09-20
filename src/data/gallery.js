91a524c33ea942ca8199b8c37f5fc9530f6d3708
AlekseyVY <alekseyvy@gmail.com>
2021-09-20 17:23:26 +0300

    feat: gallery 1920px hardcoded draft

--- src/data/gallery.js
+++ src/data/gallery.js
@@ -1,11 +1,51 @@
 
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+  return array;
+}
+
+const galleryData = shuffle(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15']);
+const galleryDTO = galleryData.map((ele) => {
+  return {
+    tag: 'img',
+    className: 'gallery-card',
+    img: `./assets/gallery/galery${ele}.jpg`,
+    alt: `galery${ele} picture`,
+    content: []
+  }
+})
+
+
 const galleryContainer = {
   tag: 'section',
   className: 'gallery-section-container',
   id: 'gallery',
-  img: null,
-  link: null,
-  content: []
+  content: [{
+    tag: 'div',
+    className: 'gallery-content-wrapper',
+    content: [{
+      tag: 'h2',
+      className: 'gallery-content-logo',
+      content: ['Art Gallery']
+    },
+      {
+        tag: 'div',
+        className: 'gallery-content-gallery',
+        content: [...galleryDTO,
+          {
+            tag: 'div',
+            className: 'gallery-content-overlay',
+            content: []
+          }]
+      },
+    ],
+  }]
 }
 
 export default galleryContainer;
