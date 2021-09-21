2610c213efa1fa4fe280986454bbd8a287f9e04a
AlekseyVY <alekseyvy@gmail.com>
2021-09-21 18:19:40 +0300

    feat: contacts 1920 hardcoded draft

--- src/data/contacts.js
+++ src/data/contacts.js
@@ -1,7 +1,64 @@
 const contactsContainer = {
   tag: 'section',
   className: 'contacts-container',
-  content: []
+  id: 'contacts',
+  content: [
+    {
+      tag: 'div',
+      className: 'contacts-content-wrapper',
+      content: [
+        {
+          tag: 'h2',
+          className: 'contacts-content-logo',
+          content: ['Contacts']
+        },
+        {
+          tag: 'div',
+          className: 'contacts-content-delimeter',
+          content: []
+        },
+        {
+          tag: 'div',
+          className: 'contacts-content-inner-wrapper',
+          content: [
+            {
+              tag: 'div',
+              className: 'contacts-content-inner-list-wrapper',
+              content: [
+                {
+                  tag: 'h3',
+                  className: 'contacts-content-inner-list-logo',
+                  content: ['Palais Royal\n Musee du Louvre']
+                },
+                {
+                  tag: 'p',
+                  className: 'contacts-content-inner-list-adress',
+                  content: ['address: 75001 Paris, France']
+                },
+                {
+                  tag: 'p',
+                  className: 'contacts-content-inner-list-phone',
+                  content: ['phone: +33(0) 1 40 20 50 50']
+                },
+                {
+                  tag: 'p',
+                  className: 'contacts-content-inner-list-mail',
+                  content: ['mail: info@louvre.fr']
+                }
+              ]
+            },
+            {
+              tag: 'img',
+              className: 'contacts-content-location',
+              img: './assets/contacts/tmp.png',
+              alt: 'location',
+              content: []
+            }
+          ]
+        }
+      ]
+    }
+  ]
 }
 
 export default contactsContainer;
