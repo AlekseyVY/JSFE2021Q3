4de9027c2d1bc486592b9c569bf723accd2a0aff
AlekseyVY <alekseyvy@gmail.com>
2021-09-28 19:46:56 +0300

    init: museum stage 2 adaptuve layout

--- src/components/createNode.js
+++ src/components/createNode.js
@@ -1,30 +1,30 @@
 
-const CreateNode = ({tag, className, id, content, link, img, blank, alt, type, lazy, name, value, placeholder, selected, disabled ,hidden, onchange}, parent) => {
-  const element = document.createElement(tag)
-  if(className) element.setAttribute('class', className);
-  if(id) element.setAttribute('id', id);
-  if(alt) element.setAttribute('alt', alt);
-  if(link) element.href = link;
-  if(lazy) element.loading = 'lazy';
-  if(blank) element.target = "_blank";
-  if(img) element.src = img;
-  if(selected) element.selected = true;
-  if(disabled) element.disabled = true;
-  if(hidden) element.hidden = true;
-  if(onchange) element.onchange = onchange;
-  if(placeholder) element.placeholder = placeholder;
-  if(value) element.value = value;
-  if(type) element.type = type;
-  if(name) element.name = name;
-  if(Array.isArray(content) && content.length > 1) {
-    content.map((ele) => {
+const CreateNode = (props, parent) => {
+  const element = document.createElement(props.tag)
+  if(props.className) element.setAttribute('class', props.className);
+  if(props.id) element.setAttribute('id', props.id);
+  if(props.alt) element.setAttribute('alt', props.alt);
+  if(props.link) element.href = props.link;
+  if(props.lazy) element.loading = 'lazy';
+  if(props.blank) element.target = "_blank";
+  if(props.img) element.src = props.img;
+  if(props.selected) element.selected = true;
+  if(props.disabled) element.disabled = true;
+  if(props.hidden) element.hidden = true;
+  if(props.onchange) element.onchange = props.onchange;
+  if(props.placeholder) element.placeholder = props.placeholder;
+  if(props.value) element.value = props.value;
+  if(props.type) element.type = props.type;
+  if(props.name) element.name = props.name;
+  if(Array.isArray(props.content) && props.content.length > 1) {
+    props.content.map((ele) => {
       CreateNode(ele, element)
     })
   } else {
-    if(typeof content[0] === 'object') {
-      CreateNode(content[0], element)
+    if(typeof props.content[0] === 'object') {
+      CreateNode(props.content[0], element)
     } else {
-      element.innerText = content;
+      element.innerText = props.content;
     }
   }
   parent.appendChild(element)
