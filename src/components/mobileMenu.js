2c143f185f61d1f9fb98d5f4ceb924fb6d144e0d
AlekseyVY <alekseyvy@gmail.com>
2021-09-29 18:42:07 +0300

    feat: welcome section stage 2 draft

--- src/components/mobileMenu.js
+++ src/components/mobileMenu.js
@@ -3,13 +3,16 @@ const mobileMenu = () => {
   const menu = document.getElementsByClassName('mobile-menu-wrapper')[0];
   const nav = document.getElementsByClassName('mobile-nav-wrapper')[0];
   const close = document.getElementsByClassName('mobile-menu-close')[0];
+  const hero = document.getElementsByClassName('hero-content')[0];
 
   menu.addEventListener('click', (e) => {
+    hero.style.display = 'none';
       nav.style.top = '130px';
       close.style.display = 'block';
       menu.style.display = 'none';
   })
   close.addEventListener('click', (e) => {
+    hero.style.display = 'block';
     nav.style.top = '-700px';
     close.style.display = 'none';
     menu.style.display = 'flex';
