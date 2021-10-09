1e65e0ebafc1ae8e2684baba115c4177e7558c4b
AlekseyVY <alekseyvy@gmail.com>
2021-10-09 20:21:15 +0300

    feat: email validation

--- src/components/validation.js
+++ src/components/validation.js
@@ -3,19 +3,37 @@
 const validation = () => {
   const name = document.getElementsByClassName('modal-form-name')[0];
   const nameError = document.getElementsByClassName('name-error')[0];
-
-
   name.addEventListener('input', (e) => {
     const test = new RegExp(/^([A-Za-z][A-Za-z]{2,14}$)|^([а-яА-ЯёЁ][а-яА-ЯёЁ]{2,14}$)/, 'gimu');
     const value = e.target.value
     if(value.match(test)) {
-      nameError.classList.add('none');
+      nameError.style.display = 'none';
+      name.style.outlineColor = 'black';
+      name.style.borderColor = 'black'
     } else {
-      nameError.classList.remove('none');
+      name.style.borderColor = 'red'
+      name.style.outlineColor = 'red';
+      nameError.style.display = 'block';
     }
-
-    // /^([A-Za-z][A-Za-z]{2,14}$)|^([а-яА-ЯёЁ][а-яА-ЯёЁ]{2,14}$)/gimu
   })
+
+  const mail = document.getElementsByClassName('modal-form-email')[0];
+  const mailError = document.getElementsByClassName('email-error')[0];
+  mail.addEventListener('input', (e) => {
+
+    const test = new RegExp(/^([A-z-0-9_]{3,15}@[A-z]{4,15}.[A-z]{2,})$/, 'gim');
+    const value = e.target.value
+    if(value.match(test)) {
+      mailError.style.display = 'none';
+      mail.style.borderColor = 'black'
+      mail.style.outlineColor = 'black';
+    } else {
+      mailError.style.display = 'block';
+      mail.style.borderColor = 'red'
+      mail.style.outlineColor = 'red';
+    }
+  })
+
 }
 
 export default validation;
