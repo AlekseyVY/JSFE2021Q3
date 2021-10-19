5cf46aea5bc65916c2fdb6c4caf0537b211d6762
AlekseyVY <alekseyvy@gmail.com>
2021-10-19 18:56:08 +0300

    feat: added quote update on interval

--- src/components/quotes/quote.worker.js
+++ src/components/quotes/quote.worker.js
@@ -1,9 +1,15 @@
 let lang = 'eng';
-
+let intervalID = null;
 self.onmessage = (e) => {
   lang = e.data;
   getQuote();
-}
+  if(intervalID) {
+    clearInterval(intervalID);
+    intervalID = setInterval(() => getQuote(), 10000);
+  } else {
+    intervalID = setInterval(() => getQuote(), 10000);
+  }
+};
 
 const getQuote = () => {
   fetch('./../assets/data/data.json')
@@ -12,7 +18,4 @@ const getQuote = () => {
       const randQuote = Math.floor(Math.random() * data[0][lang].length);
       postMessage(data[0][lang][randQuote]);
     });
-  // setTimeout(() => {
-  //   getQuote();
-  // }, 10000)
 };
