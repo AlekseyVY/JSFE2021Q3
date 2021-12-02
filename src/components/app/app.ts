0e479886d1c0fe9fe6efb5825f8b680e4b76f32b
Aleksey Vasiliyev <alekseyvy@gmail.com>
2021-12-02 19:41:50 +0300

    feat: news styles

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -12,12 +12,13 @@ class App implements IApp {
 
     start() {
         const nodeScroll = document.querySelector('body');
+        const newsNode = document.querySelector('.news') as HTMLElement;
         const node: HTMLElement = document.querySelector('.sources') as HTMLElement;
         node.addEventListener('click', (e: Event) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
         const overlay = document.createElement('div');
         overlay.addEventListener('click', () => {
-            const newsNode = document.querySelector('.news') as HTMLElement;
             newsNode.innerHTML = '';
+            newsNode.classList.remove('border');
             nodeScroll?.classList.remove('disabled');
             const overlays = Array.from(document.getElementsByClassName('overlay'));
             overlays.forEach((ele) => {
@@ -29,6 +30,7 @@ class App implements IApp {
         node.addEventListener('click', () => {
             parentNode?.append(overlay);
             nodeScroll?.classList.add('disabled');
+            newsNode.classList.add('border');
         });
         this.controller.getSources((data) => this.view.drawSources(data));
     }
