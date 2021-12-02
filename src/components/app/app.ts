1809dc9d140d3ebefa79135ca0fb01e390e83cd8
Aleksey Vasiliyev <alekseyvy@gmail.com>
2021-12-02 12:11:27 +0300

    feat: added overlay, adaptive, news in center

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -11,8 +11,25 @@ class App implements IApp {
     }
 
     start() {
+        const nodeScroll = document.querySelector('body');
         const node: HTMLElement = document.querySelector('.sources') as HTMLElement;
         node.addEventListener('click', (e: Event) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
+        const overlay = document.createElement('div');
+        overlay.addEventListener('click', () => {
+            const newsNode = document.querySelector('.news') as HTMLElement;
+            newsNode.innerHTML = '';
+            nodeScroll?.classList.remove('disabled');
+            const overlays = Array.from(document.getElementsByClassName('overlay'));
+            overlays.forEach((ele) => {
+                ele.remove();
+            });
+        });
+        overlay.classList.add('overlay');
+        const parentNode = document.querySelector('body');
+        node.addEventListener('click', () => {
+            parentNode?.append(overlay);
+            nodeScroll?.classList.add('disabled');
+        });
         this.controller.getSources((data) => this.view.drawSources(data));
     }
 }
