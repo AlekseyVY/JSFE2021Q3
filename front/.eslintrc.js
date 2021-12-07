c04e22cadd86201c5d2489c7e0b82c18e2d7a1c8
Aleksey Vasiliyev <alekseyvy@gmail.com>
2021-12-07 15:34:29 +0300

    init: initial project setup

---
+++ front/.eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ["plugin:react/recommended", "airbnb", "plugin:storybook/recommended"],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/jsx-filename-extension': [2, {
+      extensions: ['.js', '.jsx', '.ts', '.tsx']
+    }],
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'import/extensions': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-unresolved': [0, {
+      caseSensitive: false
+    }],
+    'linebreak-style': ['error', 'unix'],
+    'react/react-in-jsx-scope': 'off',
+    'react/function-component-definition': [2, {
+      namedComponents: 'arrow-function',
+      unnamedComponents: 'arrow-function'
+    }]
+  }
+};
---
+++ front/src/App.tsx
@@ -0,0 +1,3 @@
+const App = () => <div>asdsad</div>;
+
+export default App;
