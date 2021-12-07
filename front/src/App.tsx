1db2d006a133994071bfa7c2436d959d336ceec6
Aleksey Vasiliyev <alekseyvy@gmail.com>
2021-12-07 19:07:20 +0300

    feat: basic routing

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,3 +1,17 @@
-const App = () => <div>asdsad</div>;
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import NotFound from './pages/NotFound/NotFound';
+import Toys from './pages/Toys/Toys';
+
+const App = () => (
+  <div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="toys" element={<Toys />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </div>
+
+);
 
 export default App;
---
+++ front/src/pages/Toys/Toys.tsx
@@ -0,0 +1,3 @@
+const Toys = () => <div>Toys</div>;
+
+export default Toys;
