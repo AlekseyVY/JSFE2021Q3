162a8cfb54993a4b555ef9d95763244f0bd3f937
Aleksey Vasiliyev <alekseyvy@gmail.com>
2021-12-26 20:52:08 +0300

    feat: added drag false on counter === 0

--- front/src/components/TreeToy/TreeToy.tsx
+++ front/src/components/TreeToy/TreeToy.tsx
@@ -1,16 +1,24 @@
+import { useState } from 'react';
 import { Container, Image, Value } from './TreeToy.style';
 import { IProps } from '../ToyCard/ToyCard.d';
 
-const TreeToy = (ele: IProps) => (
-  <Container>
-    <Image
-      draggable
-      onDragStart={(e) => e.dataTransfer.setData('text/plain', JSON.stringify({ name: ele.num, id: String(Math.random() * 10000) }))}
-      src={`./assets/toys/${ele.num}.webp`}
-      alt={'toy'}
-    />
-    <Value>{ele.amount}</Value>
-  </Container>
-);
+const TreeToy = (ele: IProps) => {
+  const [state, setState] = useState<number>(Number(ele.amount));
+  console.log(ele);
+  return (
+    <Container>
+      <Image
+        draggable={!(state <= 0)}
+        onDragStart={(e) => {
+          e.dataTransfer.setData('text/plain', JSON.stringify({ name: ele.num, id: String(Math.random() * 10000) }));
+          setState(state - 1);
+        }}
+        src={`./assets/toys/${ele.num}.webp`}
+        alt={'toy'}
+      />
+      <Value>{state}</Value>
+    </Container>
+  );
+};
 
 export default TreeToy;
